f59024334c193def1135c1a3aa6fe7957eeaa3f9
Andres Castro <amaurok@gmail.com>
2017-04-23 22:09:11 -0500

    Fixed issues in get tax handler

--- src/main/java/org/astd/rsuite/workflow/actions/leaving/rsuite5/ProjectAtdSetClassificationVarActionHandler.java
+++ src/main/java/org/astd/rsuite/workflow/actions/leaving/rsuite5/ProjectAtdSetClassificationVarActionHandler.java
@@ -1,5 +1,6 @@
 package org.astd.rsuite.workflow.actions.leaving.rsuite5;
 
+import org.activiti.engine.delegate.Expression;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.astd.rsuite.utils.WorkflowUtils;
@@ -10,89 +11,89 @@ import com.reallysi.rsuite.api.workflow.activiti.BaseWorkflowAction;
 import com.reallysi.rsuite.api.workflow.activiti.MoListWorkflowObject;
 import com.reallysi.rsuite.api.workflow.activiti.WorkflowContext;
 
-public class ProjectAtdSetClassificationVarActionHandler 
-extends BaseWorkflowAction
-implements TempWorkflowConstants {
-	
-	/**
-     * (Optional) Alias to MO.
-     */
-    public static final String MO_ALIAS_PARAM = "moAlias";
+public class ProjectAtdSetClassificationVarActionHandler
+    extends BaseWorkflowAction
+    implements TempWorkflowConstants {
 
-    /**
-     * (Optional) Alias to MO.
-     */
-    public static final String MO_ID_PARAM = "moId";
 
-    /**
-     * Name of the variable to hold the classification xml.
-     */
-    public static final String TARGET_VAR_PARAM = "targetVariableName";
+  /**
+   * (Optional) Alias to MO.
+   */
+  public static final String MO_ALIAS_PARAM = "moAlias";
 
-	@Override
-	public void execute(WorkflowContext context) throws Exception {
-		Log wfLog = context.getWorkflowLog();
-        User user = context.getAuthorizationService().getSystemUser();
-        
-        String varName = resolveVariables(getParameter(TARGET_VAR_PARAM));
-        if (StringUtils.isBlank(varName)) {
-        	reportAndThrowRSuiteException("Target variable name not specified");
-        	
-        }
-        
-        String moId = resolveVariables(getParameter(MO_ID_PARAM));
-        if (StringUtils.isBlank(moId)) {
-        	String moAlias = resolveVariables(getParameter(MO_ALIAS_PARAM));
-        	if (!StringUtils.isBlank(moAlias)) {
-        	 	ManagedObject mo = context.getManagedObjectService().getObjectByAlias(user, moAlias);
-        	 	if (mo != null) {
-        	 		moId = mo.getId();
-        	 	}
-        	}
-        }
-        
-        if (StringUtils.isBlank(moId)) {
-        	MoListWorkflowObject moList =
-        		context.getMoListWorkflowObject();
-        	if (moList == null || moList.isEmpty()) {
-        		// There are cases where MO cannot be resolved
-        		// at this time, so we just log message and
-        		// quietly return
-        		wfLog.info(
-        		    "Unable to determine MO ID to get classification from");
-               	context.setVariable(varName, "");
-        		return;
-        	}
-        	moId = moList.getMoList().get(0).getMoid();
-        }
-        
-        String taxData = null;
-        taxData = WorkflowUtils.getClassificationXmlOfMo(
-        				context, wfLog, moId);
-        
-        // Strip out containing <RESULT> tags
-        if (taxData.startsWith("<RESULT/>")) {
-        	// No classification data for MO
-        	taxData = "";
-        } else {
-        	taxData = taxData.substring("<RESULT>".length(),
-        			taxData.length()-"</RESULT>".length());
-        }
-        
-       	wfLog.info("Classification data for "+moId+": "+taxData);
-       	context.setVariable(varName, taxData);
-	}
-	
-	public void setTargetVariableName(String s) {
-        setParameter(TARGET_VAR_PARAM, s);
+  /**
+   * (Optional) Alias to MO.
+   */
+  public static final String MO_ID_PARAM = "moId";
+
+  /**
+   * Name of the variable to hold the classification xml.
+   */
+  public static final String TARGET_VAR_PARAM = "targetVariableName";
+
+  protected Expression moAlias;
+  protected Expression targetVariableName;
+  protected Expression moId;
+
+  @Override
+  public void execute(WorkflowContext context) throws Exception {
+    Log wfLog = context.getWorkflowLog();
+    User user = context.getAuthorizationService().getSystemUser();
+
+    String varName = resolveExpression(targetVariableName);
+    if (StringUtils.isBlank(varName)) {
+      reportAndThrowRSuiteException("Target variable name not specified");
+
     }
 
-    public void setMoId(String s) {
-        setParameter(MO_ID_PARAM, s);
+    String moid = null;
+    String moalias = resolveExpression(moAlias);
+    wfLog.info("Object Alias " + moalias);
+    if (!StringUtils.isBlank(moalias)) {
+      ManagedObject mo = context.getManagedObjectService().getObjectByAlias(user, moalias);
+      if (mo != null) {
+        moid = mo.getId();
+      }
     }
 
-    public void setMoAlias(String s) {
-        setParameter(MO_ALIAS_PARAM, s);
+    if (StringUtils.isBlank(moid)) {
+      MoListWorkflowObject moList = context.getMoListWorkflowObject();
+      if (moList == null || moList.isEmpty()) {
+        // There are cases where MO cannot be resolved
+        // at this time, so we just log message and
+        // quietly return
+        wfLog.info("Unable to determine MO ID to get classification from");
+        context.setVariable(varName, "");
+        return;
+      }
+      moid = moList.getMoList().get(0).getMoid();
     }
+
+    String taxData = null;
+    taxData = WorkflowUtils.getClassificationXmlOfMo(context, wfLog, moid);
+
+    // Strip out containing <RESULT> tags
+    if (taxData.startsWith("<RESULT/>")) {
+      // No classification data for MO
+      taxData = "";
+    } else {
+      taxData = taxData.substring("<RESULT>".length(), taxData.length() - "</RESULT>".length());
+    }
+
+    wfLog.info("Classification data for " + moId + ": " + taxData);
+    context.setVariable(varName, taxData);
+  }
+
+  public void setTargetVariableName(String s) {
+    setParameter(TARGET_VAR_PARAM, s);
+  }
+
+  public void setMoId(String s) {
+    setParameter(MO_ID_PARAM, s);
+  }
+
+  public void setMoAlias(String s) {
+    setParameter(MO_ALIAS_PARAM, s);
+  }
 
 }
