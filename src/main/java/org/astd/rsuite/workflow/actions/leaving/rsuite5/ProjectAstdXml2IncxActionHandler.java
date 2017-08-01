56c3287830582384fe3e97d9224637d22ab2a8ce
Adolfo Restrepo <adolforestrepo@gmail.com>
2017-07-31 22:31:53 -0500

    created a new definition for Article to XHTML workflow. fixed some other workflow issues.

--- src/main/java/org/astd/rsuite/workflow/actions/leaving/rsuite5/ProjectAstdXml2IncxActionHandler.java
+++ src/main/java/org/astd/rsuite/workflow/actions/leaving/rsuite5/ProjectAstdXml2IncxActionHandler.java
@@ -132,138 +132,169 @@ public class ProjectAstdXml2IncxActionHandler extends BaseWorkflowAction
 	}
 
 	@Override
-  public void execute(WorkflowContext context) throws Exception {
+	public void execute(WorkflowContext context) throws Exception {
 
-    Log wfLog = context.getWorkflowLog();
+		Log wfLog = context.getWorkflowLog();
 
-    try{
-    checkParamsNotEmptyOrNull(STYLE_CATALOG_URI_PARAM, XSLT_URI_PARAM);
-    }
-    catch (Exception e) {
-    	wfLog.error("Checking parameters styleCatalogUri and xsltUriFromWorkflow");
-    	
-    }
-    String styleCatalogUriExp = resolveExpression(styleCatalogUri);
-    String xsltUriExp = resolveExpression(xsltUriFromWorkflow);
-    String fileNameExp = resolveVariablesAndExpressions(fileName.getExpressionText());
-    String xmlMoIdExp = resolveVariablesAndExpressions(xmlMoId.getExpressionText());
+		try {
+			checkParamsNotEmptyOrNull(STYLE_CATALOG_URI_PARAM, XSLT_URI_PARAM);
+		} catch (Exception e) {
+			wfLog.error("Checking parameters styleCatalogUri and xsltUriFromWorkflow");
 
+		}
+		String styleCatalogUriExp = resolveExpression(styleCatalogUri);
+		String xsltUriExp = resolveExpression(xsltUriFromWorkflow);
+		String fileNameExp = resolveVariablesAndExpressions(fileName
+				.getExpressionText());
+		String xmlMoIdExp = resolveVariablesAndExpressions(xmlMoId
+				.getExpressionText());
 
-    // bean = new XML2InDesignBean(xsltUri, styleCatalogUri);
-    bean = new XML2InDesignBean(context, xsltUriExp, styleCatalogUriExp, fileNameExp);
+		// bean = new XML2InDesignBean(xsltUri, styleCatalogUri);
+		bean = new XML2InDesignBean(context, xsltUriExp, styleCatalogUriExp,
+				fileNameExp);
 
-    File workFolder = getOutputDir(context);
+		File workFolder = getOutputDir(context);
 
-    ManagedObject mo = null;
-    if (xmlMoIdExp == null || "".equals(xmlMoIdExp)) {
-      MoListWorkflowObject moList = context.getMoListWorkflowObject();
-      if (moList == null || moList.isEmpty()) {
-        mo = context.getManagedObjectService().getObjectByAlias(context.getAuthorizationService()
-            .getSystemUser(), fileNameExp + ".xml");
-        if (mo == null) {
-          reportAndThrowRSuiteException(
-              "Failed to get an MO using the fileName parameter with the value \"" + fileNameExp
-                  + "\"");
-        }
-      } else {
-        MoWorkflowObject moObject = moList.getMoList().get(0); // First item in list should be
-                                                               // course script MO.
-        mo = context.getManagedObjectService().getManagedObject(context.getAuthorizationService()
-            .getSystemUser(), moObject.getMoid());
-        if (mo == null) {
-          reportAndThrowRSuiteException("Failed to get an MO from the workflow context");
-        }
-        // Assume first alias is a filename:
-        if (fileNameExp == null || "".equals(fileNameExp)) {
-          Alias[] aliases = mo.getAliases();
-          if (aliases.length > 0) {
-            // fileName = FilenameUtils.getBaseName(aliases[0].toString());
-            fileNameExp = FilenameUtils.getBaseName(aliases[0].getText());
-          } else {
-            fileNameExp = "article_" + mo.getId();
-          }
-        }
-      }
-    } else {
-      mo = context.getManagedObjectService().getManagedObject(context.getAuthorizationService()
-          .getSystemUser(), xmlMoIdExp);
-      if (mo == null) {
-        reportAndThrowRSuiteException("Failed to find MO with ID [" + xmlMoIdExp + "]");
-      }
-    }
+		ManagedObject mo = null;
+		if (xmlMoIdExp == null || "".equals(xmlMoIdExp)) {
+			MoListWorkflowObject moList = context.getMoListWorkflowObject();
+			if (moList == null || moList.isEmpty()) {
+				mo = context.getManagedObjectService().getObjectByAlias(
+						context.getAuthorizationService().getSystemUser(),
+						fileNameExp + ".xml");
+				if (mo == null) {
+					reportAndThrowRSuiteException("Failed to get an MO using the fileName parameter with the value \""
+							+ fileNameExp + "\"");
+				}
+			} else {
+				MoWorkflowObject moObject = moList.getMoList().get(0); // First
+																		// item
+																		// in
+																		// list
+																		// should
+																		// be
+																		// course
+																		// script
+																		// MO.
+				mo = context.getManagedObjectService().getManagedObject(
+						context.getAuthorizationService().getSystemUser(),
+						moObject.getMoid());
+				if (mo == null) {
+					reportAndThrowRSuiteException("Failed to get an MO from the workflow context");
+				}
+				// Assume first alias is a filename:
+				if (fileNameExp == null || "".equals(fileNameExp)) {
+					Alias[] aliases = mo.getAliases();
+					if (aliases.length > 0) {
+						// fileName =
+						// FilenameUtils.getBaseName(aliases[0].toString());
+						fileNameExp = FilenameUtils.getBaseName(aliases[0]
+								.getText());
+					} else {
+						fileNameExp = "article_" + mo.getId();
+					}
+				}
+			}
+		} else {
+			mo = context.getManagedObjectService().getManagedObject(
+					context.getAuthorizationService().getSystemUser(),
+					xmlMoIdExp);
+			if (mo == null) {
+				reportAndThrowRSuiteException("Failed to find MO with ID ["
+						+ xmlMoIdExp + "]");
+			}
+		}
 
-    File resultInxFile = new File(workFolder, fileNameExp + ".incx");
+		File resultInxFile = new File(workFolder, fileNameExp + ".incx");
 
-    String reportFileName = fileNameExp + "_docx2dita_at_" + getNowString() + ".txt";
-    String xformReportIdVarName = getParameterWithDefault(XFORM_REPORT_ID_VAR_NAME_PARAM,
-        XFORM_REPORT_ID_VARNAME);
-    xformReportIdVarName = resolveVariablesAndExpressions(xformReportIdVarName);
+		String reportFileName = fileNameExp + "_docx2dita_at_" + getNowString()
+				+ ".txt";
+		String xformReportIdVarName = getParameterWithDefault(
+				XFORM_REPORT_ID_VAR_NAME_PARAM, XFORM_REPORT_ID_VARNAME);
+		xformReportIdVarName = resolveVariablesAndExpressions(xformReportIdVarName);
 
+		wfLog.info("Report ID is \"" + reportFileName + "\"");
+		String xformReportFileNameVarName = getParameterWithDefault(
+				XFORM_REPORT_FILE_NAME_VAR_NAME_PARAM,
+				XFORM_REPORT_FILE_NAME_VARNAME);
+		context.setVariable(xformReportFileNameVarName, reportFileName);
 
-    wfLog.info("Report ID is \"" + reportFileName + "\"");
-    String xformReportFileNameVarName = getParameterWithDefault(
-        XFORM_REPORT_FILE_NAME_VAR_NAME_PARAM, XFORM_REPORT_FILE_NAME_VARNAME);
-    context.setVariable(xformReportFileNameVarName, reportFileName);
+		File copiedReportFile = null;
 
-    File copiedReportFile = null;
+		LoggingSaxonMessageListener logger = context.getXmlApiManager()
+				.newLoggingSaxonMessageListener(context.getWorkflowLog());
 
-    LoggingSaxonMessageListener logger = context.getXmlApiManager().newLoggingSaxonMessageListener(
-        context.getWorkflowLog());
+		boolean exceptionOccured = false;
+		try {
 
-    boolean exceptionOccured = false;
-    try {
+			// bean.generateInDesignFromTopic(mo, resultInxFile, logger);
 
-      // bean.generateInDesignFromTopic(mo, resultInxFile, logger);
+			InxGenerationOptions options = new InxGenerationOptions();
+			bean.generateInDesignFromTopic(mo.getElement().getOwnerDocument(),
+					resultInxFile, options);
 
+			// If executive summary file created, set workflow variables.
+			File resultInxExecSummaryFile = new File(workFolder, fileNameExp
+					+ "-execsum.incx");
+			if (resultInxExecSummaryFile.exists()) {
+				wfLog.info("Executive summary exists: "
+						+ resultInxExecSummaryFile);
+				String pathVarName = getParameterWithDefault(
+						EXEC_SUMMARY_PATH_VAR_NAME_PARAM,
+						DEFAULT_EXEC_SUMMARY_PATH_VAR_NAME);
+				pathVarName = resolveVariablesAndExpressions(pathVarName);
+				context.setVariable(pathVarName,
+						resultInxExecSummaryFile.getAbsolutePath());
 
-      InxGenerationOptions options = new InxGenerationOptions();
-      bean.generateInDesignFromTopic(mo.getElement().getOwnerDocument(), resultInxFile, options);
+				String fileVarName = getParameterWithDefault(
+						EXEC_SUMMARY_FILE_VAR_NAME_PARAM,
+						DEFAULT_EXEC_SUMMARY_FILE_VAR_NAME);
+				fileVarName = resolveVariablesAndExpressions(fileVarName);
+				context.setVariable(fileVarName,
+						resultInxExecSummaryFile.getName());
+			} else {
+				/* We need set the property even if there is not executive summary */
+				context.setVariable(DEFAULT_EXEC_SUMMARY_FILE_VAR_NAME, null);
+			}
+		} catch (Exception e) {
+			exceptionOccured = true;
+			reportAndThrowRSuiteException("Exception generating InDesign from mo: "
+					+ e.getLocalizedMessage());
+		} finally {
+			StringBuilder reportStr = new StringBuilder(
+					"XML to InCopy Transformation report\n\n")
+					.append("Source XML: ").append(mo.getDisplayName())
+					.append(" [").append(mo.getId()).append("]\n")
+					.append("Result file: ")
+					.append(resultInxFile.getAbsolutePath()).append("\n")
+					.append("Time performed: ").append(getNowString())
+					.append("\n\n").append(logger.getLogString()).append("\n");
+			if (exceptionOccured) {
+				reportStr
+						.append(" + [ERROR] Exception occurred: ")
+						.append(context
+								.getVariable(ProjectAstdWorkflowConstants.EXCEPTION_MESSAGE_VAR));
+			} else {
+				reportStr.append(" + [INFO] Process finished normally");
+			}
 
-      // If executive summary file created, set workflow variables.
-      File resultInxExecSummaryFile = new File(workFolder, fileNameExp + "-execsum.incx");
-      if (resultInxExecSummaryFile.exists()) {
-        wfLog.info("Executive summary exists: " + resultInxExecSummaryFile);
-        String pathVarName = getParameterWithDefault(EXEC_SUMMARY_PATH_VAR_NAME_PARAM,
-            DEFAULT_EXEC_SUMMARY_PATH_VAR_NAME);
-        pathVarName = resolveVariablesAndExpressions(pathVarName);
-        context.setVariable(pathVarName, resultInxExecSummaryFile.getAbsolutePath());
+			StoredReport report = context.getReportManager().saveReport(
+					reportFileName, reportStr.toString());
+			copiedReportFile = new File(resultInxFile.getParentFile(),
+					report.getSuggestedFileName());
+			wfLog.info("Generation report in location  \""
+					+ copiedReportFile.getAbsolutePath() + "\"");
+			context.setVariable(XFORM_REPORT_ID_VARNAME, report.getId());
+			File reportFile = report.getFile();
+			FileUtils.copyFile(reportFile, copiedReportFile);
+		}
 
-        String fileVarName = getParameterWithDefault(EXEC_SUMMARY_FILE_VAR_NAME_PARAM,
-            DEFAULT_EXEC_SUMMARY_FILE_VAR_NAME);
-        fileVarName = resolveVariablesAndExpressions(fileVarName);
-        context.setVariable(fileVarName, resultInxExecSummaryFile.getName());
-      }
-    } catch (Exception e) {
-      exceptionOccured = true;
-      reportAndThrowRSuiteException("Exception generating InDesign from mo: " + e
-          .getLocalizedMessage());
-    } finally {
-      StringBuilder reportStr = new StringBuilder("XML to InCopy Transformation report\n\n").append(
-          "Source XML: ").append(mo.getDisplayName()).append(" [").append(mo.getId()).append("]\n")
-          .append("Result file: ").append(resultInxFile.getAbsolutePath()).append("\n").append(
-              "Time performed: ").append(getNowString()).append("\n\n").append(logger
-                  .getLogString()).append("\n");
-      if (exceptionOccured) {
-        reportStr.append(" + [ERROR] Exception occurred: ").append(context.getVariable(
-            ProjectAstdWorkflowConstants.EXCEPTION_MESSAGE_VAR));
-      } else {
-        reportStr.append(" + [INFO] Process finished normally");
-      }
+		wfLog.info("XML generated in temporary file \""
+				+ resultInxFile.getAbsolutePath() + "\"");
 
-      StoredReport report = context.getReportManager().saveReport(reportFileName, reportStr
-          .toString());
-      copiedReportFile = new File(resultInxFile.getParentFile(), report.getSuggestedFileName());
-      wfLog.info("Generation report in location  \"" + copiedReportFile.getAbsolutePath() + "\"");
-      context.setVariable(XFORM_REPORT_ID_VARNAME, report.getId());
-      File reportFile = report.getFile();
-      FileUtils.copyFile(reportFile, copiedReportFile);
-    }
+		context.setFileWorkflowObject(new FileWorkflowObject(resultInxFile));
 
-    wfLog.info("XML generated in temporary file \"" + resultInxFile.getAbsolutePath() + "\"");
-
-    context.setFileWorkflowObject(new FileWorkflowObject(resultInxFile));
-
-  }
+	}
 
 	protected File getOutputDir(WorkflowContext context) throws Exception,
 			RSuiteException {
@@ -272,8 +303,12 @@ public class ProjectAstdXml2IncxActionHandler extends BaseWorkflowAction
 
 		File outputDir = null;
 		if (outputPath == null || "".equals(outputPath.trim())) {
-			/* FIXME: abetter way to set temp folder. maybe set it as a global varible */
-			outputDir = new File(context.getVariableAsString("rsuiteWorkingFolderPath"));
+			/*
+			 * FIXME: abetter way to set temp folder. maybe set it as a global
+			 * varible
+			 */
+			outputDir = new File(
+					context.getVariableAsString("rsuiteWorkingFolderPath"));
 		} else {
 			outputDir = new File(outputPath);
 			if (!outputDir.exists()) {
