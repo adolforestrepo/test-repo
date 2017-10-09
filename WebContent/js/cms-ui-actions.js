b50326d12b77af5e746d51d849f439c2116975be
Adolfo Restrepo <adolforestrepo@gmail.com>
2017-10-08 20:24:05 -0500

    iAdded jS script to redirect to the user to the Available task view.

--- WebContent/js/cms-ui-actions.js
+++ WebContent/js/cms-ui-actions.js
@@ -2,3 +2,16 @@
 // CMS UI action customizations for the ASTD project.
 //
 
+(function () {
+    // Replace this value with whatever you want to be the default.
+	var defaultBrowseCode = 'rsuite:available-tasks';
+    RSuite.model.session.addObserver('key', RSuite.model.session, function () {
+        if (this.get('key')) {
+                var ctl = RSuite.view.Activity.getController();
+                ctl.set('selectedBrowseCode', defaultBrowseCode);
+                ctl.send('publishBrowseCode');
+                window.history.pushState("object or string", "Title", "/rsuite-cms/workflow/Browse/rsuite:available-tasks");
+          }
+    });
+}());
+
